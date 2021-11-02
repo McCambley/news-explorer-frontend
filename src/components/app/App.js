2306f423d6d1bc4c19807c8656b90a12c4684593
McCambley <jakemccamb16@gmail.com>
2021-11-02 07:32:47 -0400

    Dev: style hero

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import { Switch, Route } from 'react-router-dom';
 import Hero from '../hero/Hero';
+import Header from '../header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route path="/saved-news">saved news</Route>
         <Route path="/">
---
+++ src/components/hero/styledHero.js
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import hero from '../../images/hero.png';
+
+export const Section = styled.section`
+  background-image: url(${hero});
+  background-position: center;
+  background-size: cover;
+  min-height: 800px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+`;
+
+export const Title = styled.h1`
+  font-family: 'Roboto Slab', serif;
+  font-weight: 400;
+  font-size: 60px;
+  line-height: 64px;
+  color: #fff;
+  width: 100%;
+  text-align: left;
+  padding-bottom: 32px;
+`;
+export const Subtitle = styled.h2`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: #fff;
+  width: 100%;
+  padding-bottom: 88px;
+`;
+
+export const Form = styled.form`
+  font-family: 'Roboto', sans-serif;
+  width: 100%;
+  display: flex;
+  border-radius: 100px;
+  overflow: show;
+  background-color: #fff;
+  outline: none;
+  border: 1px solid #d1d2d6;
+  backdrop-filter: blur(8px);
+  position: relative;
+  box-sizing: content-box;
+`;
+export const Input = styled.input`
+  width: 100%;
+  padding: 21px 24px;
+  outline: none;
+  border: none;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  border-radius: 100px;
+`;
+export const Button = styled.button`
+  /* width: 100%; */
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 24px;
+  padding: 20px 56px;
+  background-color: #2f71e5;
+  border: 1px solid #2f71e5;
+  color: #fff;
+  border-radius: 100px;
+  outline: none;
+  cursor: pointer;
+  box-shadow: 0px 5px 15px rgba(14, 26, 57, 0.2);
+  transition: background-color 0.15s ease, border 0.15s ease;
+  position: absolute;
+  top: -1px;
+  right: -1px;
+
+  &:hover {
+    background-color: #347eff;
+    border: 1px solid #347eff;
+  }
+  &:focus {
+    background-color: #2a65cc;
+    border: 1px solid #2a65cc;
+  }
+`;
