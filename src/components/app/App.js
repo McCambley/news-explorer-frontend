8b2e111ed31db5ebff3e8990ab4064e0a59e8430
McCambley <jakemccamb16@gmail.com>
2021-11-02 08:15:24 -0400

    Dev: continue header styling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import { Switch, Route } from 'react-router-dom';
 import Hero from '../hero/Hero';
 import Header from '../header/Header';
+import React from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
   return (
     <>
-      <Header />
+      <Header loggedIn={loggedIn} />
       <Switch>
         <Route path="/saved-news">saved news</Route>
         <Route path="/">
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import Nav from '../nav/Nav';
+import logo from '../../images/logo.svg';
+// import logoBlack from '../../images/logo-black.svg';
 
-export default function Header() {
-  return <div>Cool</div>;
+const Container = styled.header`
+  position: absolute;
+  padding: 20px 104px;
+  background: rgba(196, 196, 196, 0.01);
+  box-shadow: inset 0px -1px 0px rgba(255, 255, 255, 0.2);
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Logo = styled.img``;
+
+export default function Header({ loggedIn }) {
+  return (
+    <Container>
+      <Logo src={logo} alt="News Explorer" />
+      <Nav loggedIn={loggedIn} />
+    </Container>
+  );
 }
