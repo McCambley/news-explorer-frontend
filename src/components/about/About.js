908f255d98690e5e5f94d968bbc96230a00fd217
McCambley <jakemccamb16@gmail.com>
2021-11-02 23:19:51 -0400

    Ref: move header styles

---
+++ src/components/about/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const Container = styled.section`
+display`;
+
+export default function About() {
+  return <div></div>;
+}
