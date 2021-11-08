a6396730e2431291c6045440dad28b113d6cbf00
McCambley <jakemccamb16@gmail.com>
2021-11-08 16:59:37 -0500

    Ref: move saved card styles

---
+++ src/components/saved-card-list/styledSavedCardList.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+export const Section = styled.section`
+  background: #f5f6f7;
+`;
+
+export const Container = styled.div`
+  padding: 62px 106px;
+  max-width: 1440px;
+  margin: 0 auto;
+`;
