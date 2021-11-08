e61529869c847bd31a8ad9313ff5264e18561ec8
McCambley <jakemccamb16@gmail.com>
2021-11-08 16:49:34 -0500

    Ref: remove uneeded form styling

--- src/components/signed-up/SignedUp.js
+++ src/components/signed-up/SignedUp.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components/macro';
 import { Container, SuccessTitle, Option } from './styledSignedUp';
 
 export default function SignUp({ switchModals, show }) {
