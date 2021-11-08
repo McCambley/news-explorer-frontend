b84f2887781768bc1d08d90b437ff3930aab7570
McCambley <jakemccamb16@gmail.com>
2021-11-08 16:54:46 -0500

    Ref: move saved hero styles

--- src/components/saved-hero/SavedHero.js
+++ src/components/saved-hero/SavedHero.js
@@ -1,41 +1,8 @@
-import React, { useState } from 'react';
-import styled from 'styled-components/macro';
-
-const Section = styled.section`
-  background: #fff;
-  max-width: 1440px;
-  padding: 40px 104px;
-  margin: 0 auto;
-`;
-const Title = styled.h1`
-  font-family: 'Roboto', sans-serif;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 24px;
-  color: rgba(26, 27, 34, 0.5);
-  margin-bottom: 28px;
-`;
-const Greeting = styled.h2`
-  font-family: 'Roboto Slab', serif;
-  font-weight: 400;
-  font-size: 40px;
-  line-height: 46px;
-  max-width: 530px;
-  color: #1a1b22;
-  margin-bottom: 30px;
-`;
-const Keywords = styled.h3`
-  font-family: 'Roboto', sans-serif;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 24px;
-  color: #1a1b22;
-`;
-const List = styled.span`
-  font-weight: 700;
-`;
+import { useState } from 'react';
+import { Section, Title, Greeting, Keywords, List } from './styledSavedHero';
 
 export default function SavedHero({ savedArticles }) {
+  // updated with GET request later
   const [keywordList, setKeywordList] = useState('Nature, Yellowstone, and 2 others');
 
   return (
