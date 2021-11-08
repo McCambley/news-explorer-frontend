e191d8e8087f3d44ed17408c7008482d6fb55962
McCambley <jakemccamb16@gmail.com>
2021-11-08 16:59:37 -0500

    Ref: move saved card styles

--- src/components/saved-card-list/SavedCardList.js
+++ src/components/saved-card-list/SavedCardList.js
@@ -2,17 +2,7 @@ import React from 'react';
 import { CardList } from '../shared/styledCardList';
 import NewsCard from '../news-card/NewsCard';
 import SavedCardTop from '../saved-card-top/SavedCardTop';
-import styled from 'styled-components/macro';
-
-const Section = styled.section`
-  background: #f5f6f7;
-`;
-
-const Container = styled.div`
-  padding: 62px 106px;
-  max-width: 1440px;
-  margin: 0 auto;
-`;
+import { Section, Container } from './styledSavedCardList';
 
 export default function SavedCardList({ savedArticles }) {
   return (
