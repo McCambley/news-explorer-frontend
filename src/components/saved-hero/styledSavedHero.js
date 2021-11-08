f780931e8498c40474174315af7e848711504495
McCambley <jakemccamb16@gmail.com>
2021-11-08 16:54:46 -0500

    Ref: move saved hero styles

---
+++ src/components/saved-hero/styledSavedHero.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/macro';
+
+export const Section = styled.section`
+  background: #fff;
+  max-width: 1440px;
+  padding: 40px 104px;
+  margin: 0 auto;
+`;
+export const Title = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: rgba(26, 27, 34, 0.5);
+  margin-bottom: 28px;
+`;
+export const Greeting = styled.h2`
+  font-family: 'Roboto Slab', serif;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 46px;
+  max-width: 530px;
+  color: #1a1b22;
+  margin-bottom: 30px;
+`;
+export const Keywords = styled.h3`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  color: #1a1b22;
+`;
+export const List = styled.span`
+  font-weight: 700;
+`;
