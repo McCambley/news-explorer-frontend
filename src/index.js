a7259ea2962fd434cca6e12e0a0185e887c6e415
McCambley <jakemccamb16@gmail.com>
2021-11-09 18:47:12 -0500

    Fix: update basename for routing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <FontStyles />
-    <BrowserRouter>
+    <BrowserRouter basename="/news-explorer-frontend/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
