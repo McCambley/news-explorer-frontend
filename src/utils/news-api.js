7970d5d87b6e8ee12e099d044ead5afd6b9788e3
McCambley <jakemccamb16@gmail.com>
2021-11-13 00:44:38 -0500

    Fix: run query through proxy

--- src/utils/news-api.js
+++ src/utils/news-api.js
@@ -1,14 +1,15 @@
 class NewsApi {
   constructor() {
     this._baseUrl = 'https://newsapi.org/v2/';
+    this._proxyUrl = 'https://nomoreparties.co/news/v2/';
     this._apiKey = '48c1590798934395b1afd26cab587a67';
   }
 
   getArticles(query) {
-    return fetch(`${this._baseUrl}everything?q=${query}`, {
-      headers: {
-        'X-Api-Key': this._apiKey,
-      },
+    return fetch(`${this._proxyUrl}everything?q=${query}&apiKey=${this._apiKey}`, {
+      //   headers: {
+      //     'X-Api-Key': this._apiKey,
+      //   },
     }).then((response) => {
       if (!response.ok) {
         throw new Error(`Search failed! Status: ${response.status}`);
