e7de8c3e5dc52b7443200564f801838de8750a21
McCambley <jakemccamb16@gmail.com>
2022-02-23 12:12:22 -0500

    Ref: type theme styling

--- src/components/style/ThemeStyles.tsx
+++ src/components/style/ThemeStyles.tsx
@@ -3,8 +3,10 @@ import logoutLight from '../../images/logout-light.svg';
 import menuLight from '../../images/menu-light.svg';
 import menuDark from '../../images/menu-dark.svg';
 
+type Theme = {position: string, primary: string, secondary: string, tertiary: string, background: string, logout: string, shadow: string, hamburger: string}
+
 // header themes for home page
-export const homeTheme = {
+export const homeTheme: Theme = {
   position: 'absolute',
   primary: '#FFFFFF',
   secondary: '#B6BCBF',
@@ -16,7 +18,7 @@ export const homeTheme = {
 };
 
 // header themes for saved article page
-export const savedArticleTheme = {
+export const savedArticleTheme: Theme = {
   position: 'static',
   primary: '#1A1B22',
   secondary: '#1A1B22',
